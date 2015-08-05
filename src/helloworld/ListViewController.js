65a21788baeecab250cf52e072b34341fff768f5
DQNEO <dqneoo@gmail.com>
2015-08-05 20:30:53 +0900

    improve welcome message

--- src/helloworld/ListViewController.js
+++ src/helloworld/ListViewController.js
@@ -8,7 +8,7 @@ Package('helloworld')
         var view = new cp.View({
             template: cp.Templates.get('helloworld.ListViewController.content'),
             templateParameters: {
-                lbl: 'Hello World!'
+                lbl: 'Hello ListView!'
             }
         });
         this.setContentView(view);
