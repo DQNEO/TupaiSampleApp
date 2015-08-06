c9fc2f26e4a9c4b4b1dfaf9f6f8eceb3045ceb5f
DQNEO <dqneoo@gmail.com>
2015-08-06 15:45:26 +0900

    ajaxの結果を受け取ってゴニョゴニョするのができた！

--- src/helloworld/ResponseDelegate.js
+++ src/helloworld/ResponseDelegate.js
@@ -1,6 +1,11 @@
 Package('helloworld')
 .use('tupai.Application')
-    .define('ResponseDelegate', function(cp){ return Package.Class.extend(
-
-    );
-                                       });
+      .define('ResponseDelegate', function(cp){ return Package.Class.extend({
+          didHttpRequestSuccess: function(name, requestName, response,request){
+              console.log('did request success');
+              var resJson = JSON.parse(response.responseText);
+              console.log(resJson);
+          },
+          didHttpRequestError: function(){},
+      });
+                                              });
