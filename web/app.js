d24781c202f46b4270b2af0d896da38524810a6c
DQNEO <dqneoo@gmail.com>
2015-08-06 14:05:04 +0900

    indent to be shallower

--- web/app.js
+++ web/app.js
@@ -2,15 +2,15 @@ console.log("app.js is included");
 console.log(Package);
 
 Package()
-    .use('tupai.Application')
-    .use('Config')
-    .run(function(cp){
-      console.log('run');
-      var app = new cp.Application({
-        window: {
-          routes: cp.Config['routes']
-        },
-        cacheManager: cp.Config['cache_manager'],
-      });
-      app.show('/root');
-    });
+.use('tupai.Application')
+.use('Config')
+.run(function(cp){
+  console.log('run');
+  var app = new cp.Application({
+    window: {
+      routes: cp.Config['routes']
+    },
+    cacheManager: cp.Config['cache_manager'],
+  });
+  app.show('/root');
+});
