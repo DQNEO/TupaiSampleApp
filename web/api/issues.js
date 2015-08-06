0fba9c006c596daff912fcaf1577e24f34692f52
DQNEO <dqneoo@gmail.com>
2015-08-06 20:28:12 +0900

    データバインドできた！！

--- web/api/issues.js
+++ web/api/issues.js
@@ -1,7 +1,7 @@
 {
     "issues" :  [
-        {"id":1, "title":"foo"},
-        {"id":2, "title":"bar"},
-        {"id":4, "title":"buz"}
+        {"id":1, "title":"foo", "assignee":"DQNEO"},
+        {"id":2, "title":"bar", "assignee":"DQNEO"},
+        {"id":4, "title":"buz", "assignee":"pikachu"}
     ]
 }
